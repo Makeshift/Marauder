a402bd2ee4f6b7f5e1dceb0c9fae5db28216bfb2
Makeshift <Me@Connor-Bell.com>
2020-03-29 03:46:32 +0100

    Sightly nicer script output for Sabnzbd

--- scripts/sabnzbd_post_process_clear_rclone_vfs_cache.sh
+++ scripts/sabnzbd_post_process_clear_rclone_vfs_cache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set +xe
+set +x
 
 HOST="127.0.0.1"
 PORT="5572"
@@ -10,3 +10,8 @@ FOLDER=${FULL_FOLDER#/shared/merged/}
 echo "Telling Rclone @ ${HOST}:${PORT} to vfs/refresh directory ${FOLDER}"
 
 curl -fs -X POST "${HOST}:${PORT}/vfs/refresh?dir=${FOLDER}"
+if [ $? -eq 0 ]; then
+    echo "Rclone refreshed successfully: ${FOLDER}"
+else 
+    echo "Failed to refresh, may not be visible to client: ${FOLDER}"
+fi
