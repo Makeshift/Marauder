aff5895546207d467ca2bc61b1b8e140bfa7cbf6
Makeshift <Me@Connor-Bell.com>
2020-04-10 02:07:41 +0100

    Fixed plex rclone

--- scripts/sabnzbd_post_process_clear_rclone_vfs_cache.sh
+++ scripts/sabnzbd_post_process_clear_rclone_vfs_cache.sh
@@ -7,20 +7,9 @@ PORT="5572"
 FULL_FOLDER=$(dirname "${SAB_COMPLETE_DIR}")
 FOLDER=${FULL_FOLDER#/shared/merged/}
 
-echo "Telling Rclone @ ${HOST}:${PORT} to vfs/refresh directory ${FOLDER}"
+echo "Telling Rclone @ ${HOST}:${PORT} to refresh directory ${FOLDER}"
 
 curl -fs -X POST "${HOST}:${PORT}/vfs/refresh?dir=${FOLDER}"
-if [ $? -eq 0 ]; then
-    echo "Rclone refreshed successfully: ${FOLDER}"
-else 
-    echo "Failed to refresh, may not be visible to client: ${FOLDER}"
-fi
-
-echo "Telling Rclone @ ${HOST}:${PORT} to cache/expire ${FOLDER}"
-
 curl -fs -X POST "${HOST}:${PORT}/cache/expire?remote=${FOLDER}"
-if [ $? -eq 0 ]; then
-    echo "Rclone refreshed successfully: ${FOLDER}"
-else 
-    echo "Failed to refresh, may not be visible to client: ${FOLDER}"
-fi
+
+exit 0
