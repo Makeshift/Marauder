86d9e239c2064f793b29a33f82200008f65f7f89
Makeshift <Me@Connor-Bell.com>
2020-04-29 00:14:45 +0100

    Fixed a VFS clear issue, made cache size and upload bins configurable and tuned them a bit

--- scripts/sabnzbd_post_process_clear_rclone_vfs_cache.sh
+++ scripts/sabnzbd_post_process_clear_rclone_vfs_cache.sh
@@ -7,9 +7,8 @@ PORT="5572"
 FULL_FOLDER=$(dirname "${SAB_COMPLETE_DIR}")
 FOLDER=${FULL_FOLDER#/shared/merged/}
 
-echo "Telling Rclone @ ${HOST}:${PORT} to refresh directory ${FOLDER}"
+echo "Telling Rclone @ ${HOST}:${PORT} to forget directory ${FOLDER} and refresh it"
 
-curl -fs -X POST "${HOST}:${PORT}/vfs/refresh?dir=${FOLDER}"
-curl -fs -X POST "${HOST}:${PORT}/cache/expire?remote=${FOLDER}"
+curl -fs -X POST "${HOST}:${PORT}/vfs/forget?dir=${FOLDER}" &
 
 exit 0
